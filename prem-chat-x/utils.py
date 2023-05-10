cbbdbc8be66c8e77c351d25316af7e2d6aa95fe4
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-05-10 12:53:42 +0200

    simplified model zoo and fixed pipeline

--- prem-chat-x/utils.py
+++ prem-chat-x/utils.py
@@ -11,45 +11,26 @@ MODEL_ID = os.getenv("MODEL_ID", None)
 if MODEL_ID is None:
     raise ValueError("MODEL_ID environment variable is not set.")
 
-MODEL_ZOO = {"llama_cpp": ["gpt4all-lora-q4", "vicuna-7b-q4"]}
-
-MODELS_INFO = [
-    {
-        "id": "vicuna-7b-q4",
-        "name": "Vicuna 7B 1.1 Q4",
-        "maxLength": 12000,
-        "tokenLimit": 4000,
-        "description": "Vicuna 7B 1.1 Q4",
-        "modelWeightsName": "vicuna-7b-q4.bin",
-        "modelWeightsSize": 4212859520,
-        "modelTypes": ["chat", "embeddings"],
-        "modelDevice": "m1",
-        "modelMemoryRequirements": "8gb",
-    },
-    {
-        "id": "gpt4all-lora-q4",
-        "name": "GPT4ALL-Lora Quantized GGML",
-        "maxLength": 12000,
-        "tokenLimit": 4000,
-        "description": "GPT4ALL-Lora Quantized GGML",
-        "modelWeightsName": "gpt4all-lora-q4.bin",
-        "modelWeightsSize": 4212864640,
-        "modelTypes": ["chat", "embeddings"],
-        "modelDevice": "m1",
-        "modelMemoryRequirements": "8gb",
-    },
-]
+MODEL_ZOO = {
+    "llama_cpp": {
+        "gpt4all-lora-q4": {"modelWeightsName": "gpt4all-lora-q4.bin"},
+        "vicuna-7b-q4": {
+            "modelWeightsName": "vicuna-7b-q4.bin",
+        },
+    }
+}
 
 
 def get_model_info() -> dict:
-    for model in MODELS_INFO:
-        if model["id"] == MODEL_ID:
-            return model
+    for _, value in MODEL_ZOO.items():
+        for id, model in value.items():
+            if id == MODEL_ID:
+                return model
     raise ValueError("Model id not supported.")
 
 
 def load_model():
-    if MODEL_ID in MODEL_ZOO["llama_cpp"]:
+    if MODEL_ID in MODEL_ZOO["llama_cpp"].keys():
         from services.llamacpp import LLaMACPPBasedModel
 
         LLaMACPPBasedModel.get_model()
@@ -58,7 +39,7 @@ def load_model():
 
 
 def get_model():
-    if MODEL_ID in MODEL_ZOO["llama_cpp"]:
+    if MODEL_ID in MODEL_ZOO["llama_cpp"].keys():
         from services.llamacpp import LLaMACPPBasedModel as model
 
         return model
