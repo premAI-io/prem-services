be4c47534c5135347ea2bc3c469c9684b343dcd9
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-05-23 15:53:07 +0200

    added cpu compatibility for sentence transformers, whisper, bark

--- scripts/build_test_cpu.sh
+++ scripts/build_test_cpu.sh
@@ -1,3 +1,13 @@
+echo "Building and testing whisper based models"
+docker buildx build --file ./audio-to-text-wh/docker/cpu/Dockerfile --build-arg="MODEL_ID=tiny" --tag audio-to-text-wh ./audio-to-text-wh
+docker run --rm --name audio-to-text-wh audio-to-text-wh pytest
+
+exit
+
+echo "Building and testing sentence transformers based models"
+docker buildx build --file ./embeddings-st/docker/cpu/Dockerfile --build-arg="MODEL_ID=all-MiniLM-L6-v2" --tag embeddings-st ./embeddings-st
+docker run --rm --name embeddings-st embeddings-st pytest
+
 echo "Building and testing stable diffusion based models"
 docker buildx build --file ./michelangelo-sd/docker/cpu/Dockerfile --build-arg="MODEL_ID=stabilityai/stable-diffusion-2-1-base" --tag michelangelo-sd ./michelangelo-sd
 docker run --rm --name michelangelo-sd michelangelo-sd pytest
@@ -9,3 +19,7 @@ docker run --rm --name copilot-t5 copilot-t5 pytest
 echo "Building and testing llama_cpp based models"
 docker buildx build --file ./chat-llama-cpp/docker/cpu/Dockerfile --build-arg="MODEL_ID=vicuna-7b-q4" --tag chat-llama-cpp ./chat-llama-cpp
 docker run --rm --name chat-llama-cpp chat-llama-cpp pytest
+
+echo "Building and testing bark based models"
+docker buildx build --file ./text-to-audio-ba/docker/cpu/Dockerfile --tag text-to-audio-ba ./text-to-audio-ba
+docker run --rm --name text-to-audio-ba text-to-audio-ba pytest
--- text-to-audio-ba/download.py
+++ text-to-audio-ba/download.py
@@ -1,10 +1,4 @@
-import argparse
-
 from bark import preload_models
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--model", help="Model to download")
-args = parser.parse_args()
 
 print("Downloading model")
 
