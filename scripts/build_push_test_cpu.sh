3ada0413a63cb881e94b9c1bff1ce9b3bcb4e568
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-05-29 17:23:21 +0200

    two different scripts for building the images

---
+++ scripts/build_push_test_cpu.sh
@@ -0,0 +1,33 @@
+export VERSION=0.0.1
+
+docker system prune -f -a
+docker buildx create --use
+
+docker buildx build --push \
+    --cache-from ghcr.io/premai-io/chat-vicuna-7b-q4-cpu:latest \
+    --file ./cht-llama-cpp/docker/cpu/Dockerfile \
+    --build-arg="MODEL_ID=vicuna-7b-q4" \
+    --tag ghcr.io/premai-io/chat-vicuna-7b-q4-cpu:latest \
+    --tag ghcr.io/premai-io/chat-vicuna-7b-q4-cpu:$VERSION \
+    --platform linux/arm64,linux/amd64 ./cht-llama-cpp
+docker buildx build --push \
+    --cache-from ghcr.io/premai-io/chat-gpt4all-lora-q4-cpu:latest \
+    --file ./cht-llama-cpp/docker/cpu/Dockerfile \
+    --build-arg="MODEL_ID=gpt4all-lora-q4" \
+    --tag ghcr.io/premai-io/chat-gpt4all-lora-q4-cpu:latest \
+    --tag ghcr.io/premai-io/chat-gpt4all-lora-q4-cpu:$VERSION \
+    --platform linux/arm64,linux/amd64 ./cht-llama-cpp
+docker run --rm ghcr.io/premai-io/chat-vicuna-7b-q4-cpu:latest pytest
+
+docker system prune -f -a
+
+docker buildx build --push \
+    --cache-from ghcr.io/premai-io/embeddings-all-minilm-l6-v2-cpu:latest \
+    --file ./ebd-all-minilm/docker/cpu/Dockerfile \
+    --build-arg="MODEL_ID=all-MiniLM-L6-v2" \
+    --tag ghcr.io/premai-io/embeddings-all-minilm-l6-v2-cpu:latest \
+    --tag ghcr.io/premai-io/embeddings-all-minilm-l6-v2-cpu:$VERSION \
+    --platform linux/arm64,linux/amd64 ./ebd-all-minilm
+docker run --rm --gpus all ghcr.io/premai-io/embeddings-all-minilm-l6-v2-cpu:latest pytest
+
+docker system prune -f -a
---
+++ scripts/build_push_test_gpu.sh
@@ -0,0 +1,27 @@
+export VERSION=0.0.1
+
+docker system prune -f -a
+docker buildx create --use
+
+docker buildx build --push \
+    --cache-from ghcr.io/premai-io/chat-dolly-v2-12b-gpu:latest \
+    --file ./cht-dolly-v2/docker/gpu/Dockerfile \
+    --build-arg="MODEL_ID=databricks/dolly-v2-12b" \
+    --tag ghcr.io/premai-io/chat-dolly-v2-12b-gpu:latest \
+    --tag ghcr.io/premai-io/chat-dolly-v2-12b-gpu:$VERSION \
+    --platform linux/amd64 ./cht-dolly-v2
+docker run --gpus all ghcr.io/premai-io/chat-dolly-v2-12b-gpu:latest pytest
+
+docker system prune -f -a
+
+docker buildx build --push \
+    --cache-from ghcr.io/premai-io/embeddings-all-minilm-l6-v2-gpu:latest \
+    --file ./ebd-all-minilm/docker/gpu/Dockerfile \
+    --build-arg="MODEL_ID=all-MiniLM-L6-v2" \
+    --tag ghcr.io/premai-io/embeddings-all-minilm-l6-v2-gpu:latest \
+    --tag ghcr.io/premai-io/embeddings-all-minilm-l6-v2-gpu:$VERSION \
+    --platform linux/amd64 ./ebd-all-minilm
+docker run --rm ghcr.io/premai-io/embeddings-all-minilm-l6-v2-cpu:latest pytest
+docker run --rm --gpus all ghcr.io/premai-io/embeddings-all-minilm-l6-v2-gpu:latest pytest
+
+docker system prune -f -a
