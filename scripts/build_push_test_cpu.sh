345f0ed63c49e2abc18a601ff331506fb54bcc31
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-06-22 16:34:03 +0200

    added t5 coder

--- scripts/build_push_test_cpu.sh
+++ scripts/build_push_test_cpu.sh
@@ -29,18 +29,6 @@ docker buildx build --push \
     --platform linux/arm64,linux/amd64 ./ebd-all-minilm
 docker run --rm ghcr.io/premai-io/embeddings-all-minilm-l6-v2-cpu:latest pytest
 
-docker system prune -f -a
-
-docker buildx build --push \
-    --cache-from ghcr.io/premai-io/coder-codet5p-220m-py-cpu:latest \
-    --file ./cdr-t5/docker/cpu/Dockerfile \
-    --build-arg="MODEL_ID=Salesforce/codet5p-220m-py" \
-    --tag ghcr.io/premai-io/coder-codet5p-220m-py-cpu:latest \
-    --tag ghcr.io/premai-io/coder-codet5p-220m-py-cpu:$VERSION \
-    --platform linux/amd64 ./cdr-t5
-
-docker run --rm ghcr.io/premai-io/coder-codet5p-220m-py-cpu:latest pytest
-
 docker buildx create --use
 docker buildx build --push \
     --cache-from=ghcr.io/premai-io/text-to-audio-bark-cpu:latest \
