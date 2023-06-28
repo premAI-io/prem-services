d9e8a1dc52630da546cf54c4d5ea2b4bde27ad6e
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-06-28 10:15:47 +0000

    dfs fix

--- scripts/dfs_diffusers.sh
+++ scripts/dfs_diffusers.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-export VERSION=1.0.0
+export VERSION=1.0.2
 
 docker buildx build --push \
-    --cache-from=ghcr.io/premai-io/diffuser-stable-diffusion-2-1-base-gpu:latest \
+    --cache-from=ghcr.io/premai-io/diffuser-stable-diffusion-2-1-gpu:latest \
     --file ./dfs-diffusers/docker/gpu/Dockerfile \
-    --build-arg="MODEL_ID=stabilityai/stable-diffusion-2-1-base" \
-    --tag ghcr.io/premai-io/diffuser-stable-diffusion-2-1-base-gpu:latest \
-    --tag ghcr.io/premai-io/diffuser-stable-diffusion-2-1-base-gpu:$VERSION \
+    --build-arg="MODEL_ID=stabilityai/stable-diffusion-2-1" \
+    --tag ghcr.io/premai-io/diffuser-stable-diffusion-2-1-gpu:latest \
+    --tag ghcr.io/premai-io/diffuser-stable-diffusion-2-1-gpu:$VERSION \
     ./dfs-diffusers
 
 docker buildx build --push \
@@ -23,35 +23,9 @@ docker buildx build --push \
 docker buildx build --push \
     --cache-from=ghcr.io/premai-io/diffuser-stable-diffusion-2-base-gpu:latest \
     --file ./dfs-diffusers/docker/gpu/Dockerfile \
-    --build-arg="MODEL_ID=stabilityai/stable-diffusion-2-base" \
-    --tag ghcr.io/premai-io/diffuser-stable-diffusion-2-base-gpu:latest \
-    --tag ghcr.io/premai-io/diffuser-stable-diffusion-2-base-gpu:$VERSION \
+    --build-arg="MODEL_ID=stabilityai/stable-diffusion-2" \
+    --tag ghcr.io/premai-io/diffuser-stable-diffusion-2-gpu:latest \
+    --tag ghcr.io/premai-io/diffuser-stable-diffusion-2-gpu:$VERSION \
     ./dfs-diffusers
 
-docker run --rm --gpus all ghcr.io/premai-io/diffuser-stable-diffusion-2-base-gpu:$VERSION pytest
-
-docker buildx build --push \
-    --cache-from=ghcr.io/premai-io/diffuser-stable-diffusion-2-1-base-cpu:latest \
-    --file ./dfs-diffusers/docker/cpu/Dockerfile \
-    --build-arg="MODEL_ID=stabilityai/stable-diffusion-2-1-base" \
-    --tag ghcr.io/premai-io/diffuser-stable-diffusion-2-1-base-cpu:latest \
-    --tag ghcr.io/premai-io/diffuser-stable-diffusion-2-1-base-cpu:$VERSION \
-    --platform linux/arm64,linux/amd64 ./dfs-diffusers
-
-docker buildx build --push \
-    --cache-from=ghcr.io/premai-io/diffuser-stable-diffusion-2-base-cpu:latest \
-    --file ./dfs-diffusers/docker/cpu/Dockerfile \
-    --build-arg="MODEL_ID=stabilityai/stable-diffusion-2-base" \
-    --tag ghcr.io/premai-io/diffuser-stable-diffusion-2-base-cpu:latest \
-    --tag ghcr.io/premai-io/diffuser-stable-diffusion-2-base-cpu:$VERSION \
-    --platform linux/arm64,linux/amd64 ./dfs-diffusers
-
-docker buildx build --push \
-    --cache-from=ghcr.io/premai-io/diffuser-stable-diffusion-1-5-cpu:latest \
-    --file ./dfs-diffusers/docker/cpu/Dockerfile \
-    --build-arg="MODEL_ID=runwayml/stable-diffusion-v1-5" \
-    --tag ghcr.io/premai-io/diffuser-stable-diffusion-1-5-cpu:latest \
-    --tag ghcr.io/premai-io/diffuser-stable-diffusion-1-5-cpu:$VERSION \
-    --platform linux/arm64,linux/amd64 ./dfs-diffusers
-
-docker run --rm ghcr.io/premai-io/diffuser-stable-diffusion-2-base-cpu:$VERSION pytest
+docker run --rm --gpus all ghcr.io/premai-io/diffuser-stable-diffusion-2-gpu:$VERSION pytest
