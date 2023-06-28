54000970cfad7797695f165830a8f6f49a5a6221
Biswaroop Bhattacharjee <biswaroop08@gmail.com>
2023-06-29 01:12:01 +0530

    update: script for falcon-7b-instruct image

--- scripts/cht_falcon.sh
+++ scripts/cht_falcon.sh
@@ -5,10 +5,10 @@ set -e
 export VERSION=1.0.0
 
 docker buildx build --push \
-    --cache-from ghcr.io/premai-io/chat-falcon-7b-gpu:latest \
+    --cache-from ghcr.io/premai-io/chat-falcon-7b-instruct-gpu:latest \
     --file ./cht-falcon/docker/gpu/Dockerfile \
-    --build-arg="MODEL_ID=tiiuae/falcon-7b" \
-    --tag ghcr.io/premai-io/chat-falcon-7b-gpu:latest \
-    --tag ghcr.io/premai-io/chat-falcon-7b-gpu:$VERSION \
+    --build-arg="MODEL_ID=tiiuae/falcon-7b-instruct" \
+    --tag ghcr.io/premai-io/chat-falcon-7b-instruct-gpu:latest \
+    --tag ghcr.io/premai-io/chat-falcon-7b-instruct-gpu:$VERSION \
     ./cht-falcon
-docker run --gpus all ghcr.io/premai-io/chat-falcon-7b-gpu:latest pytest
+docker run --gpus all ghcr.io/premai-io/chat-falcon-7b-instruct-gpu:latest pytest
