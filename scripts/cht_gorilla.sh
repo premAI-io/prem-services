a4d49eb65eb8704852d5c9f4352226c6d4234cce
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-07-05 17:26:42 +0000

    updated naming in docker image

--- scripts/cht_gorilla.sh
+++ scripts/cht_gorilla.sh
@@ -5,10 +5,10 @@ set -e
 export VERSION=1.0.0
 
 docker buildx build --push \
-    --cache-from ghcr.io/premai-io/chat-gorilla-7b-hf-gpu:latest \
+    --cache-from ghcr.io/premai-io/chat-gorilla-7b-gpu:latest \
     --file ./cht-gorilla/docker/gpu/Dockerfile \
     --build-arg="MODEL_ID=gorilla-llm/gorilla-falcon-7b-hf-v0" \
-    --tag ghcr.io/premai-io/chat-gorilla-7b-hf-gpu:latest \
-    --tag ghcr.io/premai-io/chat-gorilla-7b-hf-gpu:$VERSION \
+    --tag ghcr.io/premai-io/chat-gorilla-7b-gpu:latest \
+    --tag ghcr.io/premai-io/chat-gorilla-7b-gpu:$VERSION \
     ./cht-gorilla
-docker run --gpus all ghcr.io/premai-io/chat-gorilla-7b-hf-gpu:latest pytest
+docker run --gpus all ghcr.io/premai-io/chat-gorilla-7b-gpu:latest pytest
