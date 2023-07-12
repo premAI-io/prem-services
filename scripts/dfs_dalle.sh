ce84ac1c8fd297121ff3cffc264b4778cbb49bc5
Biswaroop Bhattacharjee <biswaroop08@gmail.com>
2023-07-12 18:24:58 +0530

    update: dalle image uri

--- scripts/dfs_dalle.sh
+++ scripts/dfs_dalle.sh
@@ -5,14 +5,14 @@ set -e
 export VERSION=1.0.0
 
 docker buildx build --push \
-    --cache-from=ghcr.io/premai-io/dalle-mini-gpu:latest \
+    --cache-from=ghcr.io/premai-io/diffuser-dalle-mini-gpu:latest \
     --file ./dfs-dalle/docker/gpu/Dockerfile \
     --build-arg="DALLE_MODEL_ID=dalle-mini/dalle-mini" \
     --build-arg="DALLE_REVISION_ID=''" \
     --build-arg="VQGAN_MODEL_ID=dalle-mini/vqgan_imagenet_f16_16384" \
     --build-arg="VQGAN_REVISION_ID=e93a26e7707683d349bf5d5c41c5b0ef69b677a9" \
-    --tag ghcr.io/premai-io/dalle-mini-gpu:latest \
-    --tag ghcr.io/premai-io/dalle-mini-gpu:$VERSION \
+    --tag ghcr.io/premai-io/diffuser-dalle-mini-gpu:latest \
+    --tag ghcr.io/premai-io/diffuser-dalle-mini-gpu:$VERSION \
     ./dfs-dalle
 
-docker run --rm --gpus all ghcr.io/premai-io/dalle-mini-gpu:latest pytest
+docker run --rm --gpus all ghcr.io/premai-io/diffuser-dalle-mini-gpu:latest pytest
