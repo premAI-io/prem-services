b048249986fccfb696aab97dc48bcbe67c1bafa3
Biswaroop Bhattacharjee <biswaroop08@gmail.com>
2023-07-12 14:32:03 +0530

    update: removed cpu for dalle-mini

--- scripts/dfs_dalle.sh
+++ scripts/dfs_dalle.sh
@@ -15,15 +15,4 @@ docker buildx build --push \
     --tag ghcr.io/premai-io/dalle-mini-gpu:$VERSION \
     ./dfs-dalle
 
-docker buildx build --push \
-    --cache-from=ghcr.io/premai-io/dalle-mini-cpu:latest \
-    --file ./dfs-dalle/docker/cpu/Dockerfile \
-    --build-arg="DALLE_MODEL_ID=dalle-mini/dalle-mini" \
-    --build-arg="DALLE_REVISION_ID=''" \
-    --build-arg="VQGAN_MODEL_ID=dalle-mini/vqgan_imagenet_f16_16384" \
-    --build-arg="VQGAN_REVISION_ID=e93a26e7707683d349bf5d5c41c5b0ef69b677a9" \
-    --tag ghcr.io/premai-io/dalle-mini-cpu:latest \
-    --tag ghcr.io/premai-io/dalle-mini-cpu:$VERSION \
-    --platform linux/arm64,linux/amd64 ./dfs-dalle
-
 docker run --rm --gpus all ghcr.io/premai-io/dalle-mini-gpu:latest pytest
