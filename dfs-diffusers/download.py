f7c461633157a639ba05a03a9ed8f38127622c73
biswaroop1547 <biswaroop08@gmail.com>
2023-07-18 20:49:23 +0000

    fix: dependencies and model downloads

--- dfs-diffusers/download.py
+++ dfs-diffusers/download.py
@@ -1,7 +1,7 @@
 import argparse
 import torch
 
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline, StableDiffusionLatentUpscalePipeline
 from tenacity import retry, stop_after_attempt, wait_fixed
 
 parser = argparse.ArgumentParser()
@@ -12,7 +12,10 @@ print(f"Downloading model {args.model}")
 
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(5))
 def download_model():
-    _ = StableDiffusionPipeline.from_pretrained(args.model, torch_dtype=torch.float32)
+    if "latent" not in args.model:
+        _ = StableDiffusionPipeline.from_pretrained(args.model, torch_dtype=torch.float16)
+    _ = StableDiffusionLatentUpscalePipeline.from_pretrained(args.model, torch_dtype=torch.float16)
+    
 
 
 download_model()
