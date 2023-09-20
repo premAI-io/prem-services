d1ba3457b27a7571853b1b1c16de6b19f3dd6288
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-09-20 09:01:09 +0200

    updated model ids

--- cht-petals/build.sh
+++ cht-petals/build.sh
@@ -3,5 +3,5 @@ set -e
 export VERSION=1.0.4
 source "$(dirname "${BASH_SOURCE[0]}")/../utils.sh"
 
-build_cpu ghcr.io/premai-io/chat-codellama-34b-cpu   codellama-34b   ${@:1}
-build_cpu ghcr.io/premai-io/chat-stable-beluga-2-cpu stable-beluga-2 ${@:1}
+build_cpu ghcr.io/premai-io/chat-codellama-34b-cpu   premai-io/CodeLlama-34b-Instruct-hf ${@:1}
+build_cpu ghcr.io/premai-io/chat-stable-beluga-2-cpu petals-team/StableBeluga2           ${@:1}
