4fac336f076c68a97dcba9dfcb3d436b3eec3d25
Casper da Costa-Luis <casper.dcl@physics.org>
2023-09-21 13:17:00 +0100

    some tidy

--- cht-petals/download.py
+++ cht-petals/download.py
@@ -1,10 +1,10 @@
 import argparse
+import platform
 
 import torch
 from petals import AutoDistributedModelForCausalLM
 from tenacity import retry, stop_after_attempt, wait_fixed
 from transformers import AutoTokenizer, LlamaTokenizer
-from utils import get_cpu_architecture
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", help="Model to download")
@@ -15,17 +15,13 @@ print(f"Downloading model {args.model}")
 
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(5))
 def download_model() -> None:
-    if "llama" in args.model.lower():
-        _ = LlamaTokenizer.from_pretrained(args.model)
-    else:
-        _ = AutoTokenizer.from_pretrained(args.model)
+    Tokenizer = LlamaTokenizer if "llama" in args.model.lower() else AutoTokenizer
+    _ = Tokenizer.from_pretrained(args.model)
 
-    if get_cpu_architecture() == "AMD":
-        _ = AutoDistributedModelForCausalLM.from_pretrained(
-            args.model, torch_dtype=torch.float32
-        )
-    else:
-        _ = AutoDistributedModelForCausalLM.from_pretrained(args.model)
+    kwargs = {}
+    if "x86_64" in platform.machine():
+        kwargs["torch_dtype"] = torch.float32
+    _ = AutoDistributedModelForCausalLM.from_pretrained(args.model, **kwargs)
 
 
 download_model()
