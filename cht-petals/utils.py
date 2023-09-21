d5f13fe5a55078d9a4b42bff038cc19cf723d17c
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-09-21 10:15:58 +0200

    debugging build

--- cht-petals/utils.py
+++ cht-petals/utils.py
@@ -3,6 +3,7 @@ import platform
 
 def get_cpu_architecture():
     arch = platform.machine()
+    print(f"Detected CPU architecture: {arch}", flush=True)
     if "x86_64" in arch:
         return "AMD"
     elif "arm" in arch:
