21874ed653bd744ba5ce0d2549f3532f350d9f8e
Casper da Costa-Luis <casper.dcl@physics.org>
2023-10-18 11:48:26 +0100

    fix macos aarch64 release paths

--- cht-llama-cpp/build-aarch64-apple-darwin.sh
+++ cht-llama-cpp/build-aarch64-apple-darwin.sh
@@ -7,11 +7,11 @@ source ./venv/bin/activate
 pip install -r requirements.txt pyinstaller
 LLAMA_CPP_PATH=$(python -c 'import llama_cpp; print(llama_cpp.__path__[0])')
 # macOS (dylib) package
-NAME=mistral-${VERSION}-aarch64-apple-darwin
+NAME=cht-llama-cpp-mistral-${VERSION}-aarch64-apple-darwin
 pyinstaller --onefile \
   --target-arch arm64 \
   --add-binary "$LLAMA_CPP_PATH/libllama.dylib:llama_cpp" \
   --name=$NAME \
   --paths ./venv/lib/python${1:-3.11}/site-packages \
   main.py
-cp dist/$NAME dist/mistral-${VERSION%%.*}-aarch64-apple-darwin
+cp dist/$NAME dist/cht-llama-cpp-mistral-${VERSION%%.*}-aarch64-apple-darwin
