dbacb000dac086922207685baeab08845f28c429
Casper da Costa-Luis <casper.dcl@physics.org>
2023-10-18 09:29:22 +0100

    update --clobber release

--- cht-llama-cpp/build-aarch64-apple-darwin.sh
+++ cht-llama-cpp/build-aarch64-apple-darwin.sh
@@ -7,15 +7,11 @@ source ./venv/bin/activate
 pip install -r requirements.txt pyinstaller
 LLAMA_CPP_PATH=$(python -c 'import llama_cpp; print(llama_cpp.__path__[0])')
 # macOS (dylib) package
-NAME=mistral-aarch64-apple-darwin
+NAME=mistral-${VERSION}-aarch64-apple-darwin
 pyinstaller --onefile \
   --target-arch arm64 \
   --add-binary "$LLAMA_CPP_PATH/libllama.dylib:llama_cpp" \
   --name=$NAME \
   --paths ./venv/lib/python3.11/site-packages \
   main.py
-
-if test -f $GITHUB_OUTPUT; then
-  echo "bin_name=$NAME" >> $GITHUB_OUTPUT
-  echo "bin_path=${PWD}/dist/$NAME" >> $GITHUB_OUTPUT
-fi
+cp dist/$NAME dist/mistral-${VERSION%%.*}-aarch64-apple-darwin
