4736c77d8eb20e9f617623cb1190597a0c796434
biswaroop1547 <biswaroop08@gmail.com>
2023-10-20 07:01:09 +0530

    update: new bash script for managing installation and starting of cht-petals service without docker

--- cht-petals/main.py
+++ cht-petals/main.py
@@ -1,4 +1,6 @@
+import argparse
 import logging
+import os
 
 import uvicorn
 from dotenv import load_dotenv
@@ -7,6 +9,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from routes import router as api_router
 
 load_dotenv()
+
+MODEL_PATH = "./models"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", help="Path to Model files directory", default=MODEL_PATH)
+    parser.add_argument("--dht_prefix", help="DHT prefix to use")
+    args = parser.parse_args()
+    MODEL_PATH = args.model_path
+    DHT_PREFIX = args.dht_prefix
 
 logging.basicConfig(
     format="%(asctime)s %(levelname)-8s %(message)s",
@@ -19,7 +30,7 @@ def create_start_app_handler(app: FastAPI):
     def start_app() -> None:
         from models import PetalsBasedModel
 
-        PetalsBasedModel.get_model()
+        PetalsBasedModel.get_model(MODEL_PATH, DHT_PREFIX)
 
     return start_app
 
