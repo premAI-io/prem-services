9bec015737df12c231acaee646c6e23118aa5e10
Casper da Costa-Luis <casper.dcl@physics.org>
2023-10-23 19:53:33 +0100

    fix git clone tmpdir

--- cht-petals/setup-petals.sh
+++ cht-petals/setup-petals.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # ./setup-petals.sh --model-path ./models/models--petals-team--StableBeluga2 --dht-prefix StableBeluga2-hf --port 8794
 
-tmpdir="$(mktemp)"
+tmpdir="${PREM_APPDIR:-.}/$(uuid)"
 
 # clone source
 git clone -n --depth=1 --filter=tree:0 https://github.com/premAI-io/prem-services.git "$tmpdir"
 git -C "$tmpdir" sparse-checkout set --no-cone cht-petals
 git -C "$tmpdir" checkout
 # install deps
-"${PREM_PYTHON:-python}" -m pip install -r "$tmpdir/cht-petals/requirements.txt"
+"${PREM_PYTHON:-python}" -m pip install -r "$tmpdir/cht-petals/requirements.txt" || :
 # run server
-PYTHONPATH="$tmpdir/cht-petals" "${PREM_PYTHON:-python}" "$tmpdir/cht-petals/main.py" "$@"
+PYTHONPATH="$tmpdir/cht-petals" "${PREM_PYTHON:-python}" "$tmpdir/cht-petals/main.py" "$@" || :
 
 rm -r "$tmpdir"
