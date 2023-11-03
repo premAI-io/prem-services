e8ba0c3b5888214ce74b63b2fbda42b982e7f042
Casper da Costa-Luis <casper.dcl@physics.org>
2023-11-03 12:41:03 +0000

    slight tidy

--- ebd-all-minilm/build-aarch64-apple-darwin.sh
+++ ebd-all-minilm/build-aarch64-apple-darwin.sh
@@ -1,33 +1,28 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 set -e
-
-export VERSION=1.0.5
+export VERSION=1.0.4
 
 test -f venv/bin/activate || python -m venv venv
 source venv/bin/activate
 
-
 pip install -r requirements.txt pyinstaller
 NAME=ebd-all-minilm-${VERSION}-aarch64-apple-darwin
 pyinstaller --onefile \
-  --copy-metadata tqdm \
+  --copy-metadata filelock \
+  --copy-metadata huggingface-hub \
+  --copy-metadata numpy \
+  --copy-metadata packaging \
+  --copy-metadata pyyaml \
   --copy-metadata regex \
   --copy-metadata requests \
-  --copy-metadata packaging \
-  --copy-metadata filelock \
-  --copy-metadata numpy \
-  --copy-metadata huggingface-hub \
   --copy-metadata safetensors \
-  --copy-metadata pyyaml \
+  --copy-metadata tokenizers \
+  --copy-metadata tqdm \
   --hidden-import=pytorch \
   --collect-data torch \
   --copy-metadata torch \
-  --add-data 'main.py:.' \
-  --copy-metadata tokenizers \
   --hidden-import=tiktoken_ext.openai_public \
   --hidden-import=tiktoken_ext \
-  --target-arch arm64 \
   --name=$NAME \
   main.py
 cp dist/$NAME dist/ebd-all-minilm-${VERSION%%.*}-aarch64-apple-darwin
